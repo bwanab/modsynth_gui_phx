f0a753a65a92e26eaafb7a4838dc00bfa3a65508
Bill Allen <bill@k2bea.org>
2025-07-16 14:38:06 -0400

    set mix.exs to use github and rebuild mix.lock

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule ModsynthGuiPhx.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:sc_em, path: "../sc_em"}
+      # {:sc_em, path: "../sc_em"}
+      {:sc_em, github: "bwanab/sc_em"}
     ]
   end
 
