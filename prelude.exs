21981c628fa79095453e9b903592ba66e21162a7
Bill Allen <bill@k2bea.org>
2025-07-26 09:19:00 -0400

    Add bpm to prelude helper function. Update mix.lock for dependencies.

--- prelude.exs
+++ prelude.exs
@@ -3,13 +3,17 @@
 # Helper functions to make scripts more user-friendly
 
 # Simple helper to create STrack with default values
-  def make_arpeggios(patterns, chord_syms, root \\ :C, octave \\ 3, duration \\ 0.5, repeats \\ 1) do
+  def make_arpeggios(patterns, chord_syms, opts \\ []) do
+    root = Keyword.get(opts, :root, :C)
+    octave = Keyword.get(opts, :octave, 3)
+    duration = Keyword.get(opts, :duration, 0.5)
+    repeats = Keyword.get(opts, :repeats, 1)
     chords = MusicBuild.Examples.ArpeggioProgressions.build_chords(chord_syms, root, octave, duration, 0)
     Enum.map(Enum.zip(chords, patterns), fn {c, p} -> Arpeggio.new(c, p, duration, 0) end)
             |> List.duplicate(repeats)
             |> List.flatten
   end
 
-  def strack(sonorities) do
-    %{0 => STrack.new(sonorities, name: "", tpqn: 960, type: :instrument, program_number: 10, bpm: 100)}
+  def strack(sonorities, bpm \\ 100) do
+    %{0 => STrack.new(sonorities, name: "", tpqn: 960, type: :instrument, program_number: 10, bpm: bpm)}
   end
