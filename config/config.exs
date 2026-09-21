2f67ccf54df9b000429b51e4294e21ce2a1586fd
Bill Allen <bill@k2bea.org>
2026-09-21 17:44:44 -0400

    old file

--- config/config.exs
+++ config/config.exs
@@ -61,7 +61,7 @@ config :tailwind,
   ]
 
 # Configures Elixir's Logger
-config :logger, :console, level: :info,
+config :logger, :console, level: :debug,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id, :file, :line]
 
